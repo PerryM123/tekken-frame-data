055bcfb0a388e8ae5778a6af0a5a719c67fd9829
perry.miranda <perrymiranda1@yahoo.com>
2023-02-26 13:42:31 +0900

    github pagesのURLを修正

--- utils/getHostName.ts
+++ utils/getHostName.ts
@@ -1,6 +1,15 @@
 // TODO: 単体テストを追加
-export default function getHostName(): string {
+export function getHostName(): string {
   const GITPAGES_URL: string = process.env.GITPAGES_URL || '';
   const LOCAL_URL: string = process.env.LOCAL_URL || '';
   return process.env.NODE_ENV === 'production' ? GITPAGES_URL : LOCAL_URL;
+}
+
+// TODO: 単体テストを追加
+export function getContentServerHostName(): string {
+  const GITPAGES_CONTENT_URL: string = process.env.GITPAGES_CONTENT_URL || '';
+  const LOCAL_URL: string = process.env.LOCAL_URL || '';
+  return process.env.NODE_ENV === 'production'
+    ? GITPAGES_CONTENT_URL
+    : LOCAL_URL;
 }
